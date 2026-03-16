46d5af20238d19f61dfed699272f606c69d27d12
luka-kyz <anok49010@gmail.com>
2026-03-16 17:00:11 +0100

    ewewew

---
+++ index.js
@@ -0,0 +1,46 @@
+// 1
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// 2
+// for (let i = 2; i <= 20; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+
+// 3
+// const number = 7;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${number} * ${i} = ${number * i}`);
+// }
+
+// 4 
+// const n = 10;
+
+// for (let i = 1; ; i++) {
+//   if (i >= n) {
+//     break;
+//   }
+
+//   console.log(i);
+// }
+
+// 5
+// let i = 1;
+
+// while (i <= 20) {
+//   if (i % 3 === 0) {
+//     i++;
+//     continue;
+//   }
+
+//   console.log(i);
+//   i++;
+// }
